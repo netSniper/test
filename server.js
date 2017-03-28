3cf63fa41e9bf85bec5c88fbbc4f8e126745b751
呼噜娃 <422123725@qq.com>
2017-03-28 17:38:20 +0800

    注释添加

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ http.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.write('<h1>Node.js</h1>');
 res.end('<p>Hello World</p>');
-}).listen(3000);
+}).listen(5000);
+//添加注释
 console.log("HTTP server is listening at port 5000.");
 console.log(123);
 
