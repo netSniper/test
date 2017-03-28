005dc63eec94961dd92ae0ad8e61a7c926c4a90f
呼噜娃 <422123725@qq.com>
2017-03-28 16:25:07 +0800

    日志修改

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ res.write('<h1>Node.js</h1>');
 res.end('<p>Hello World</p>');
 }).listen(3000);
 console.log("HTTP server is listening at port 5000.");
-console.log(321313);
+console.log(123);
 
 
 
