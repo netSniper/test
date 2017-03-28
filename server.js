e79ecb35ee3c28b691653cbb1f7f3f864191853d
呼噜娃 <422123725@qq.com>
2017-03-28 16:19:53 +0800

    3000改为5000端口

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ res.writeHead(200, {'Content-Type': 'text/html'});
 res.write('<h1>Node.js</h1>');
 res.end('<p>Hello World</p>');
 }).listen(3000);
-console.log("HTTP server is listening at port 3000.");
+console.log("HTTP server is listening at port 5000.");
 console.log(321313);
 
 
